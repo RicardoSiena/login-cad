3624845c225365b7c93a29a963a038286f307b3a
RICARDO MATHEUS CORREA SIENA <ricardo.siena@alunoescolassociais.org.br>
2024-08-02 11:51:10 -0300

    Faz com que o nome inserido no array seja listado

--- controller.js
+++ controller.js
@@ -22,9 +22,20 @@ function salvarUser(){
     
     if(nomeUser){
         dadosLista.push(nomeUser);
-        console.log(dadosLista);
+      //console.log(dadosLista);
+      criarlista()
         document.getElementById('nomeUser').value = "";
     }else{
         alert('Favor informar o nome para cadastro');
+    }
+}
+
+// FUNÇÃO PARA CRIAR LISTA
+
+function criarlista(){
+    let tabela = document.getElementById ('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>"
+    for(let i = 0; i <= (dadosLista.length-1) ; i++){
+        tabela += "<tr><td>" + dadosLista [i] + "</td><td></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
     }
 }
